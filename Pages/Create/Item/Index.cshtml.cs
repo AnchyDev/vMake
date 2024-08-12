efca9486da771e8d717ec5a71a31839590d6a456
AnchyDev <lewienato@gmail.com>
2024-08-13 09:35:15 +1000

    Add better error handling.

--- Pages/Create/Item/Index.cshtml.cs
+++ Pages/Create/Item/Index.cshtml.cs
@@ -12,43 +12,54 @@ public class IndexModel : PageModel
     public string? Error { get; set; }
 
     private readonly MangosDbContext dbContext;
+    private readonly ILogger<IndexModel> logger;
 
-    public IndexModel(MangosDbContext dbContext)
+    public IndexModel(MangosDbContext dbContext, ILogger<IndexModel> logger)
     {
         this.dbContext = dbContext;
+        this.logger = logger;
     }
 
     public async Task<IActionResult> OnPostAsync([FromForm]int? entry)
     {
-        if(entry is null)
+        try
         {
-            Error = "You must fill in an entry id to create.";
+            if (entry is null)
+            {
+                Error = "You must fill in an entry id to create.";
+                return Page();
+            }
+
+            var template = await dbContext.ItemTemplate.FirstOrDefaultAsync(i => i.Entry == entry);
+            if (template is not null)
+            {
+                Error = "An item with that entry id already exists.";
+                return Page();
+            }
+
+            var newItem = new MangosItemTemplate()
+            {
+                Entry = entry.Value,
+                Name = "New Item",
+                Description = "This is your new item!"
+            };
+
+            var item = await dbContext.ItemTemplate.AddAsync(newItem);
+
+            var rows = await dbContext.SaveChangesAsync();
+            if (rows < 1)
+            {
+                Error = "An unknown error occured while trying to create the item.";
+                return Page();
+            }
+
+            return RedirectToPage("/Edit/Item/Index", new { entry = entry });
+        }
+        catch(Exception ex)
+        {
+            logger.LogCritical($"{ex.Message}: {ex.StackTrace}");
+            Error = $"An internal error occured while trying to create the item: {ex.Message}";
             return Page();
         }
-
-        var template = await dbContext.ItemTemplate.FirstOrDefaultAsync(i => i.Entry == entry);
-        if(template is not null)
-        {
-            Error = "An item with that entry id already exists.";
-            return Page();
-        }
-
-        var newItem = new MangosItemTemplate()
-        {
-            Entry = entry.Value,
-            Name = "New Item",
-            Description = "This is your new item!"
-        };
-
-        var item = await dbContext.ItemTemplate.AddAsync(newItem);
-
-        var rows = await dbContext.SaveChangesAsync();
-        if(rows < 1)
-        {
-            Error = "An unknown error occured while trying to create the item.";
-            return Page();
-        }
-
-        return RedirectToPage("/Edit/Item/Index", new { entry = entry });
     }
 }
--- Pages/Edit/Item/Index.cshtml.cs
+++ Pages/Edit/Item/Index.cshtml.cs
@@ -15,29 +15,40 @@ public class IndexModel : PageModel
     public MangosItemTemplate? ItemTemplate { get; set; }
 
     private readonly MangosDbContext dbContext;
+    private readonly ILogger<IndexModel> logger;
 
-    public IndexModel(MangosDbContext dbContext)
+    public IndexModel(MangosDbContext dbContext, ILogger<IndexModel> logger)
     {
         this.dbContext = dbContext;
+        this.logger = logger;
     }
 
     public async Task<IActionResult> OnGetAsync(int? entry)
     {
-        if (entry is null)
+        try
         {
+            if (entry is null)
+            {
+                return Page();
+            }
+
+            var itemTemplate = await dbContext.ItemTemplate.FirstOrDefaultAsync(i => i.Entry == entry);
+            if (itemTemplate is null)
+            {
+                Error = "An item with that entry id does not exist.";
+                return Page();
+            }
+
+            ItemTemplate = itemTemplate;
+
             return Page();
         }
-
-        var itemTemplate = await dbContext.ItemTemplate.FirstOrDefaultAsync(i => i.Entry == entry);
-        if (itemTemplate is null)
+        catch(Exception ex)
         {
-            Error = "An item with that entry id does not exist.";
+            logger.LogCritical($"{ex.Message}: {ex.StackTrace}");
+            Error = $"An internal error occured while trying to get the item: {ex.Message}";
             return Page();
         }
-
-        ItemTemplate = itemTemplate;
-
-        return Page();
     }
 
     public async Task<IActionResult> OnPostAsync(string? type, int? entry)
@@ -62,20 +73,29 @@ public class IndexModel : PageModel
 
     private async Task<IActionResult> UpdateEntryAsync()
     {
-        if(ItemTemplate is null)
+        try
         {
+            if (ItemTemplate is null)
+            {
+                return Page();
+            }
+
+            dbContext.ItemTemplate.Update(ItemTemplate);
+
+            var rows = await dbContext.SaveChangesAsync();
+            if (rows < 1)
+            {
+                Error = "An unknown error occured while trying to update the item.";
+                return Page();
+            }
+
+            return RedirectToPage("/Edit/Item/Index", new { entry = ItemTemplate.Entry });
+        }
+        catch(Exception ex)
+        {
+            logger.LogCritical($"{ex.Message}: {ex.StackTrace}");
+            Error = $"An internal error occured while trying to update the item: {ex.Message}";
             return Page();
         }
-
-        dbContext.ItemTemplate.Update(ItemTemplate);
-
-        var rows = await dbContext.SaveChangesAsync();
-        if(rows < 1)
-        {
-            Error = "An unknown error occured while trying to update the item.";
-            return Page();
-        }
-
-        return RedirectToPage("/Edit/Item/Index", new { entry = ItemTemplate.Entry });
     }
 }
