be0d02ca23b3daa05f58805250b007a2a2a78fda
AnchyDev <lewienato@gmail.com>
2024-08-17 11:21:09 +1000

    Added sell price.

---
+++ Database/Types/MangosCurrency.cs
@@ -0,0 +1,25 @@
+﻿namespace vMake.Database.Types;
+
+public struct MangosCurrency
+{
+    public int Gold { get; set; }
+    public int Silver { get; set; }
+    public int Copper { get; set; }
+
+    public static MangosCurrency ToCurrency(int copper)
+    {
+        const int copperPerSilver = 100;
+        const int silverPerGold = 100;
+
+        int gold = copper / (silverPerGold * copperPerSilver);
+        int silver = (copper % (silverPerGold * copperPerSilver)) / copperPerSilver;
+        copper = copper % copperPerSilver;
+
+        return new MangosCurrency()
+        {
+            Gold = gold,
+            Silver = silver,
+            Copper = copper
+        };
+    }
+}
