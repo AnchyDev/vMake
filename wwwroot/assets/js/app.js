1706e880a977f9541857e5c17aeeeb70690a6bdb
AnchyDev <lewienato@gmail.com>
2024-08-20 10:47:44 +1000

    Added navmenu button to minimize the nav.

---
+++ wwwroot/assets/js/app.js
@@ -0,0 +1,19 @@
+﻿let navToggled = false;
+
+function toggleNav() {
+    let textElements = document.getElementsByClassName('sidebar-navlink-text');
+    let toggleElement = document.getElementById('sidebar-toggle');
+
+    navToggled = !navToggled;
+
+    for (const element of textElements) {
+        if (navToggled) {
+            element.style.display = 'none';
+            toggleElement.className = 'bi bi-arrow-bar-right sidebar-navlink-icon';
+        }
+        else {
+            element.style.display = 'flex';
+            toggleElement.className = 'bi bi-arrow-bar-left sidebar-navlink-icon';
+        }
+    }
+}
