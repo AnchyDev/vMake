f52b26d162a208db034733498156df4ffb78ddd9
AnchyDev <lewienato@gmail.com>
2024-08-27 09:56:20 +1000

    Added quest visualizer to quest editor.

--- Components/QuestEditor/QuestEditor.razor.cs
+++ Components/QuestEditor/QuestEditor.razor.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
+using System.Text;
+using System.Text.Json;
+
+using vMake.Database;
 using vMake.Models;
 
 namespace vMake.Components.QuestEditor;
@@ -8,4 +13,56 @@ public partial class QuestEditor
 {
     [Parameter]
     public MakeQuestTemplate Template { get; set; } = default!;
+
+    [Inject]
+    private MangosDbContext DbContext { get; set; } = default!;
+
+    public string? Export { get; private set; }
+
+    public string Status { get; set; } = "";
+
+    private enum QuestEditorTab
+    {
+        General,
+        Save
+    }
+
+    private QuestEditorTab currentTab = QuestEditorTab.General;
+
+    private async void UpdateQuestAsync()
+    {
+        try
+        {
+            // Re-attach the cloned entity so we can update the original.
+            var entity = DbContext.Attach(Template.QuestTemplate);
+            entity.State = EntityState.Modified;
+
+            await DbContext.SaveChangesAsync();
+
+            // Detach the entity again as we do not want to track it.
+            entity.State = EntityState.Detached;
+
+            Status = "Saved changes.";
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Status = $"An error occured during save: {ex.Message}";
+        }
+    }
+
+    private void ExportTemplate()
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(Template);
+            Export = Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Status = $"Failed to generate export string: {ex.Message}";
+        }
+    }
 }
---
+++ Components/QuestEditor/QuestLog.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using vMake.Models;
+
+namespace vMake.Components.QuestEditor;
+
+public partial class QuestLog
+{
+    [Parameter]
+    public MakeQuestTemplate Template { get; set; } = default!;
+}
